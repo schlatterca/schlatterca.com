8fc817f05ff7463fb9e49d8515a30c019b74348a
schlatterca <48732275+schlatterca@users.noreply.github.com>
2020-12-28 22:43:35 +0100

    Update pushButton.js

--- js/pushButton.js
+++ js/pushButton.js
@@ -1,10 +1,12 @@
-let red = 0;
-let green = 255;
-
 let count = 1;
+let colorCount = 1;
+let projectCount = 1;
 let opac_2 = 1;
 let opac_3 = 1;
 let opac_4 = 1;
+
+let root = document.documentElement;
+
 /*var nome = ["Schlatter Carlo Andrea", "@schlatterca su Instagram", 
                 "Carlito Andreito e poi Schlatterito", "Anche detto Andrea Carlo",
                 "Scachler, Sacher, Screecher, dai lui", "Anche detto Swiss Army Man",
@@ -18,7 +20,7 @@ let opac_4 = 1;
                 "Lui, insomma, non lui, l'altro", "Non ce l'hai presente?"];*/
 var nome = ["Carlo Andrea Schlatter", "Instagram: <a target='_blank' href='https://www.instagram.com/schlatter.ca/'>@schlatterca</a>",
 				"Facebook: <a target='_blank' href='https://www.facebook.com/c.a.schlatter'>Carlo Schlatter</a>", "Mail: <a target='_blank' href='mailto: schlatterca@gmail.com'>schlatterca@gmail.com</a>",]
-var impiego = ["studente di grafica<br>e comunicazione", "copywriter freelance<br>per belle aziende",
+var impiego = ["studente di Graphic Design", "copywriter freelance<br>per belle aziende",
 				"appassionato di enigmistica", "grande fan di Fabri Fibra",
 				"cuoco improvvisato ma pulito", "patito della politica americana",
 				"intenditore di caramelle<br>gommose e cioccolata", "amatore a livello amatoriale",
@@ -27,7 +29,7 @@ var impiego = ["studente di grafica<br>e comunicazione", "copywriter freelance<b
 				"fotografo soltanto<br>dopo il tramonto", "filosofo per tre anni a Bologna",
 				"accarezzatore di idee<br>strane e bislacche", "domatore di leoni nani",
 				"curatore di bonsai giganti", "sul libro paga della Chiquita"]
-var rightnow = ["è laureato in Filosofia<br>all'Università di Bologna", "ha studiato al Liceo<br>Classico di Cesena",
+var rightnow = ["ISIA Urbino, Italia", "laureato in Filosofia<br>all'Università di Bologna",
 				"viene da Gambettola,<br>Forlì-Cesena", "è in viaggio verso est",
 				"è al lavoro sul suo sito", "sta giocando a Metal Slug",
 				"immerso in un libro triste", "all'ascolto di rap italiano",
@@ -37,16 +39,24 @@ var rightnow = ["è laureato in Filosofia<br>all'Università di Bologna", "ha st
 				"sta scrivendo un’autobiografia", "sta salvando il Natale",
 				"lo stanno incoronando<br>Papa proprio adesso", "è ai casting di Masterchef",
 				"è stato rapito dagli alieni", "è in giro con Massimo<br>Pericolo e tutta la gang"]
-var activity = ["di recente ha fatto:", "guarda mamma che bravo:",
+var activity = ["tra i suoi ultimi lavori:", "guarda mamma che bravo:",
 				"vedi anche alla voce:", "chiudi gli occhi e clicca:",
-				"NON È UN VIRUS:", "tocca qui per essere felice:",
+				"NON È UN VIRUS:", "si è dedicato a:",
 				"enlarge your graphics:", "non si vergogna affatto di:",
 				"crede che dovresti vedere:", "il suo progetto preferito:",
 				"è un po’ ateo, ma crede in:", "con gli amici si vanta di:",
-				"ha lavorato fino a tardi per:", "tiene come un figlio a:",
+				"ha lavorato fino a tardi per:", "tiene come a un figlio a:",
 				"forse potrebbe piacerti:", "inizia a conoscerlo da:",
 				"potrebbe spiegarti tutto su:", "un vecchio saggio gli ispirò:",
-				"ha visto in sogno:", "tornato da Narnia ha fatto:"]
+				"ha visto in sogno:", "tornato da Narnia ha fatto:",
+				"vorrebbe venderti i diritti di:", "dai un’occhiata a:"]
+var colori = ["rgb(30,140,255,.8)", "rgb(150,55,155,.8)",
+				"rgb(225,55,55,.8)", "rgb(245,130,0,.8)",
+				"rgb(255,185,0,.8)", "rgb(95,190,60,.8)"]
+var projects = ["Deep Fusion", "Parade 2020", "Urbino Esposta",
+				"Filtri RA x Studio Dallas", "Hayao Typeface",
+				"Dorli Typeface", "Via Pascucci Spazio 14",
+				"Unconventional Posting"]
 
 function pushButton_1() {
 	/*red += 50;
@@ -66,84 +76,109 @@ function pushButton_1() {
         document.getElementsByClassName("nome")[0].classList.remove("rotation");
     }, 300);
 
+    if (document.getElementsByClassName("nome")[0].innerHTML == "Carlo Andrea Schlatter") {
+		document.getElementsByClassName("nome")[0].style.background = "rgb(30,140,255,.8)";
+	} else {
+		document.getElementsByClassName("nome")[0].style.background = "rgb(80, 215, 105,.8)";
+	}
+
 }
 
 function pushButton_2() {
-	if (opac_2 > 0.09){
-    	opac_2 -= 0.03;
-    	document.getElementsByClassName("impiego")[0].style.opacity = opac_2;
-		document.getElementsByClassName("impiego")[0].innerHTML = impiego[Math.floor(Math.random() * impiego.length)];
+	document.getElementsByClassName("impiego")[0].innerHTML = impiego[Math.floor(Math.random() * impiego.length)];
 
-		document.getElementsByClassName("impiego")[0].classList.add("rotation");
-    	setTimeout(function() {
-        	document.getElementsByClassName("impiego")[0].classList.remove("rotation");
-  		}, 300);
-    } else {
-    	document.getElementsByClassName("impiego")[0].style.opacity = 1;
-    	document.getElementsByClassName("impiego")[0].innerHTML = "studente di Graphic Design";
-    	document.getElementsByClassName("impiego")[0].style.color = "rgb(30,30,30)";
-    	document.getElementsByClassName("impiego")[0].style.background = "none";
-    }
+	document.getElementsByClassName("impiego")[0].classList.add("rotation");
+	setTimeout(function() {
+    	document.getElementsByClassName("impiego")[0].classList.remove("rotation");
+		}, 300);
+
+	if (document.getElementsByClassName("impiego")[0].innerHTML == "studente di Graphic Design") {
+		document.getElementsByClassName("impiego")[0].style.background = "rgb(30,140,255,.8)";
+	} else {
+		document.getElementsByClassName("impiego")[0].style.background = "rgb(80, 215, 105,.8)";
+	}
 }
 
 function pushButton_3() {
-	if (opac_3 > 0.09){
-    	opac_3 -= 0.03;
-    	document.getElementsByClassName("rightnow")[0].style.opacity = opac_3;
-		document.getElementsByClassName("rightnow")[0].innerHTML = rightnow[Math.floor(Math.random() * rightnow.length)];
+	document.getElementsByClassName("rightnow")[0].innerHTML = rightnow[Math.floor(Math.random() * rightnow.length)];
 
-		document.getElementsByClassName("rightnow")[0].classList.add("rotation");
-    	setTimeout(function() {
-        	document.getElementsByClassName("rightnow")[0].classList.remove("rotation");
-    	}, 300);
-    } else {
-    	document.getElementsByClassName("rightnow")[0].style.opacity = 1;
-    	document.getElementsByClassName("rightnow")[0].innerHTML = "ISIA Urbino, Italia";
-    	document.getElementsByClassName("rightnow")[0].style.color = "rgb(30,30,30)";
-    	document.getElementsByClassName("rightnow")[0].style.background = "none";
-    }
+	document.getElementsByClassName("rightnow")[0].classList.add("rotation");
+	setTimeout(function() {
+    	document.getElementsByClassName("rightnow")[0].classList.remove("rotation");
+		}, 300);
+
+	if (document.getElementsByClassName("rightnow")[0].innerHTML == "ISIA Urbino, Italia") {
+		document.getElementsByClassName("rightnow")[0].style.background = "rgb(30,140,255,.8)";
+	} else {
+		document.getElementsByClassName("rightnow")[0].style.background = "rgb(80, 215, 105,.8)";
+	}
 }
 
 function pushButton_4() {
-	if (opac_4 > 0.09){
-    	opac_4 -= 0.03;
-    	document.getElementsByClassName("activity")[0].style.opacity = opac_4;
-		document.getElementsByClassName("activity")[0].innerHTML = activity[Math.floor(Math.random() * activity.length)];
+	document.getElementsByClassName("activity")[0].innerHTML = activity[Math.floor(Math.random() * activity.length)];
 
-		document.getElementsByClassName("activity")[0].classList.add("rotation");
-    	setTimeout(function() {
-        	document.getElementsByClassName("activity")[0].classList.remove("rotation");
-    	}, 300)
-    } else {
-    	document.getElementsByClassName("activity")[0].style.opacity = 1;
-    	document.getElementsByClassName("activity")[0].innerHTML = "dai un’occhiata:";
-    	document.getElementsByClassName("activity")[0].style.color = "rgb(30,30,30)";
-    	document.getElementsByClassName("activity")[0].style.background = "none";
-    }
+	document.getElementsByClassName("activity")[0].classList.add("rotation");
+	setTimeout(function() {
+    	document.getElementsByClassName("activity")[0].classList.remove("rotation");
+		}, 300);
+
+	if (document.getElementsByClassName("activity")[0].innerHTML == "tra i suoi ultimi lavori:") {
+		document.getElementsByClassName("activity")[0].style.background = "rgb(30,140,255,.8)";
+	} else {
+		document.getElementsByClassName("activity")[0].style.background = "rgb(80, 215, 105,.8)";
+	}
 }
 
 function loadOpacity() {
+	document.getElementById("underName").children[0].innerHTML = projects[Math.floor(Math.random() * projects.length)];
+	if (document.getElementById("underName").children[0].innerHTML == "Parade 2020") {
+		root.style.setProperty('--gifUrl', "url('../img/PA.gif')")
+	} else if (document.getElementById("underName").children[0].innerHTML == "Deep Fusion"){
+		root.style.setProperty('--gifUrl', "url('https://digitazon.com/wp-content/uploads/2020/05/60f6e7294309c3ec67855e35eb1912da.gif')")
+	} else if (document.getElementById("underName").children[0].innerHTML == "Urbino Esposta"){
+		root.style.setProperty('--gifUrl', "url('https://lh3.googleusercontent.com/proxy/EgYmobsn4jM30I84CZ809RzRVCuOYmEHnUdXSIEBt6Xvg2zEOka6SFoyybemb8mWgod1xfrKkSLrEauN_rZye6Tx2YgHLTr7GvHx7AI1uaaDFn0Y8E0c5tt63SVgFCc')")
+	} else {
+		root.style.setProperty('--gifUrl', "url('https://www.verdict.co.uk/wp-content/uploads/2017/09/giphy-downsized-large.gif')")
+	}
+
+
+
 	document.getElementsByClassName("nome")[0].classList.add("makeAppear_1");
 	setTimeout(function() {
         	document.getElementsByClassName("nome")[0].classList.remove("makeAppear_1");
 			document.getElementsByClassName("nome")[0].style.opacity = 1;
-    	}, 400);
+    	}, 300);
 
 	document.getElementsByClassName("impiego")[0].classList.add("makeAppear_2");
 	setTimeout(function() {
         	document.getElementsByClassName("impiego")[0].classList.remove("makeAppear_2");
 			document.getElementsByClassName("impiego")[0].style.opacity = 1;
-    	}, 800);
+    	}, 600);
 
 	document.getElementsByClassName("rightnow")[0].classList.add("makeAppear_3");
 	setTimeout(function() {
         	document.getElementsByClassName("rightnow")[0].classList.remove("makeAppear_3");
         	document.getElementsByClassName("rightnow")[0].style.opacity = 1;
-    	}, 1200);
+    	}, 900);
 
 	document.getElementsByClassName("activity")[0].classList.add("makeAppear_4");
 	setTimeout(function() {
         	document.getElementsByClassName("activity")[0].classList.remove("makeAppear_4");
         	document.getElementsByClassName("activity")[0].style.opacity = 1;
-    	}, 1600);
+    	}, 1200);
+}
+
+function reloadPage() {
+	location = location;
+}
+
+function changeColors() {
+	
+	root.style.setProperty('--mainColor', colori[colorCount]);
+
+	if (colorCount < colori.length-1) {
+		colorCount += 1;
+	} else {
+		colorCount = 0;
+	}
 }
