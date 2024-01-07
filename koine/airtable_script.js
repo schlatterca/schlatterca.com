9aec9a502dea161af99dd663166b97295f392dda
Carlo <48732275+schlatterca@users.noreply.github.com>
2024-01-07 13:12:07 +0100

    Add files via upload

---
+++ koine/airtable_script.js
@@ -0,0 +1,119 @@
+const octokit = new Octokit({
+  auth: 'YOUR-TOKEN'
+})
+
+await octokit.request('GET /repos/schlatterca/schlatterca.com/actions/secrets/SECRET_APIKEY', {
+    owner: 'schlatterca',
+    repo: 'schlatterca.com',
+    secret_name: 'SECRET_APIKEY',
+    headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+}, function done(success) {
+    console.log(success);
+});
+
+
+var Airtable = require('airtable');
+// Get a base ID for an instance of art gallery example
+var base = new Airtable({ apiKey: 'pattXNrg1dENKpkj9.f14b70198486e0fd16929cabdcbbdc199a487d68ca638f4f08a3a54a99bb313d' }).base('appPGNwsWUArcPG3U');
+
+let index = 0;
+var loadArtists = function() {
+    $('#words').empty();
+
+    base('database_A').select({
+        sort: [
+            {field: 'parola', direction: 'asc'}
+        ]
+    }).eachPage(function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+
+            //CHECK IF HAS ARTEFACT
+            if (record.get('parola') === undefined){
+                return;
+            }
+
+            //LIMIT TO THREE RECORDS
+            index += 1;
+            /*if (index > 3) {
+                return;
+            }*/
+
+            //console.log('Retrieved ', record.get('title'));
+
+            //console.log(record.get('definizione'));
+
+            var $artistInfo = $('<div>');
+
+            if(record.get('definizione') != undefined) {
+                $artistInfo.append($('<p class="unclickable">').text(record.get('parola')));
+            } else {
+                $artistInfo.append($('<p onclick=openModal(this)>').text(record.get('parola')));
+            }
+
+
+            $artistInfo.attr('data-record-id', record.getId());
+
+            $('#words').append($artistInfo);
+        });
+
+        fetchNextPage();
+
+    }, function done(error) {
+        //console.log(error);
+    });
+};
+
+loadArtists();
+
+
+/*MORE*/
+function openModal(myWord) {
+	let myWordText = myWord.innerText;
+    let myWordId = myWord.parentElement.getAttribute('data-record-id');
+
+	$('#modal').removeAttr("hidden");
+	$('#modal p.chosenWord')[0].innerText = myWordText;
+    $('#modal p.chosenWord')[0].setAttribute('data-record-id', myWordId);
+}
+
+function closeModal() {
+	$('#modal').attr("hidden",true);
+}
+
+function sendDefinition(definit) {
+
+    let myWord = $(definit).parent().find('.chosenWord')[0].innerText;
+    let myId = $(definit).parent().find('.chosenWord')[0].getAttribute('data-record-id');
+    let myDefinition = $(definit).siblings('#definition').val();
+    let myName = $(definit).siblings('#nome').val();
+
+	/*console.log(myWord);
+	console.log(myId);
+	console.log(myDefinition);
+    console.log(myName.length);*/
+
+    //console.log(new Date());
+
+    base('database_A').update([
+        {
+          "id": myId,
+          "fields": {
+            "definizione": myDefinition,
+            "autore": myName,
+            "data": new Date()
+          }
+        }
+      ], function(err, records) {
+        if (err) {
+          console.error(err);
+          return;
+        } else {
+            location.reload();
+        }
+    });
+
+    closeModal();
+
+}
